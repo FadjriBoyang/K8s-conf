73e44e850e3b3924bb33d547340a745eda99db93
FadjriBoyang <117616974+FadjriBoyang@users.noreply.github.com>
2024-01-15 08:33:24 +0700

    dhwhad

--- Kubeadm/masterv2.sh
+++ Kubeadm/masterv2.sh
@@ -43,9 +43,11 @@ kubectl taint nodes --all node-role.kubernetes.io/control-plane-
 
 # Install Claico Network Plugin Network 
 
-kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/tigera-operator.yaml
 
-curl https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/custom-resources.yaml -O
 
-kubectl create -f custom-resources.yaml
+#kubectl create -f https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/tigera-operator.yaml
+
+#curl https://raw.githubusercontent.com/projectcalico/calico/v3.26.1/manifests/custom-resources.yaml -O
+
+#kubectl create -f custom-resources.yaml
 # sudo kubeadm init --control-plane-endpoint="$MASTER_PUBLIC_IP" --apiserver-cert-extra-sans="$MASTER_PUBLIC_IP" --pod-network-cidr="10.244.0.0/16" --node-name "$NODENAME" --ignore-preflight-errors Swap --v=5
